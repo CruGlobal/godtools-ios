be1378b01d3801a70e7b36105798f84d817990f0
Harro <michael.harrison@cru.org>
2014-03-13 16:58:02 -0400

    New Project

---
+++ godtools/GTAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  GTAppDelegate.h
+//  godtools
+//
+//  Created by Michael Harrison on 3/13/14.
+//  Copyright (c) 2014 Michael Harrison. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GTAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ godtools/GTViewController.h
@@ -0,0 +1,13 @@
+//
+//  GTViewController.h
+//  godtools
+//
+//  Created by Michael Harrison on 3/13/14.
+//  Copyright (c) 2014 Michael Harrison. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GTViewController : UIViewController
+
+@end
