8ddd1b0aaa7dce2a3c0be1260aac0a2e9c520430
Harro <michael.harrison@cru.org>
2014-05-28 19:19:15 -0400

    Updated documentation for CRUStorage.

--- godtools/godtools-ios-api/Persistence/CRUStorage.h
+++ godtools/godtools-ios-api/Persistence/CRUStorage.h
@@ -9,39 +9,61 @@
 #import <Foundation/Foundation.h>
 #import <CoreData/CoreData.h>
 
+/**
+ *  Name for 'Could not open persistent store' exception.
+ */
 extern NSString *const CRUStorageExceptionNameForCouldNotOpenStore;
+/**
+ *  Key for UserInfo that accompanies exceptions from this class.
+ */
 extern NSString *const CRUStorageExceptionUserInfoKeyForError;
 
+/**
+ *  Core Data stack. Based on http://www.objc.io/issue-10/networked-core-data-application.html
+ *  This Class will quickly setup a main object context and a background object context which can either have shared
+ *  or separate persistent stores based on how you will use it.
+ */
 @interface CRUStorage : NSObject
 
 /**
- @param storeURL - URL of sqlite file. Usually you put it in the Application Documents folder.
- This method will create it if it doesn't exist.
- @param modelURL - URL of managed object model file. Usually apart of the main bundle.
- @param shared - determines whether each context gets its own store coordinator or whether they share a store coordinator. Sharing a store coordinator means a shared cache but more locking so is good for cases where there are fewer writes. Separate store coordinators mean less locking but no shared cache so is good for cases with lots of changes. Refer to http://www.objc.io/issue-10/networked-core-data-application.html
- 
- @warning If there is an issue with opening the persistent store an exception will be thrown with name set to  CRUStorageExceptionNameForCouldNotOpenStore
- */
+*  Creates and configures Core Data stack based on method parameters.
+*
+*  @param storeURL URL of sqlite file. Usually you put it in the Application Documents folder. This method will create it if it doesn't exist.
+*  @param modelURL URL of managed object model file. Usually apart of the main bundle.
+*  @param shared   determines whether each context gets its own store coordinator or whether they share a store coordinator. Sharing a store coordinator means a shared cache but more locking so is good for cases where there are fewer writes. Separate store coordinators mean less locking but no shared cache so is good for cases with lots of changes. Refer to http://www.objc.io/issue-10/networked-core-data-application.html for more details
+*
+*  @warning If there is an issue with opening the persistent store an exception will be thrown with name set to  CRUStorageExceptionNameForCouldNotOpenStore
+*
+*  @return configured storage object
+*/
 - (id)initWithStoreURL:(NSURL*)storeURL modelURL:(NSURL*)modelURL contextsSharePersistentStoreCoordinator:(BOOL)shared;
 
 /**
- The context that works on the main thread. This should be used for fetch request that update the UI. If you are updating the database with a bunch of data at a time then use the background context.
+ *  The context that works on the main thread. This should be used for fetch request that update the UI.
+ *  If you are updating the database with a bunch of data at a time then use the background context.
  */
 @property (nonatomic,readonly) NSManagedObjectContext* mainObjectContext;
 
 /**
- Anything put into the background context will automatically be merged into the main context once the background context is saved. So please save batches of data (especially from web services) to this context and do your fetch requests on the main context.
+ *  The context that works in the background. Good for importing data from a webservice.
+ *
+ *  @note Anything put into the background context will automatically be merged into the main context once the background context is saved.
+ *  So please save batches of data (especially from web services) to this context and do your fetch requests on the main context.
  */
 @property (nonatomic,readonly) NSManagedObjectContext* backgroundObjectContext;
 
 /**
- Will grab an array of Model Objects from the Persistent Store where their value for key matches one of the IDs in IDsArray
- @param modelType - Class of object you want to grab (assumes Class name matches CoreData Entity name)
- @param key - The name of the instance variable on the Model that you would like to search. It will return models with IDs that match the value on myModel.myKey in IDsArray[]
- @param valueArray - array of IDs that you would like to search for.
- @param background - should it be run on the background context (using its background queue) or the main context (using the main thread)
- @example [myStorage fetchArrayOfModels:[Book class] usingKey:@"title" forIDs:@[@"Gone with the wind", @"The Bible"] inBackground:YES];
- this would search in the background for books with the title "Gone with the wind" or "The Bible".
+ *  Grab an array of Model Objects from the Persistent Store. It will only grab objects where their value for key matches one of the IDs in valueArray
+ *
+ *  @param modelType  Class of object you want to grab (assumes Class name matches CoreData Entity name)
+ *  @param key        The name of the property on the Model that you would like to search. ie is myModel.myKey in valueArray[]
+ *  @param valueArray array of values that you would like to search for.
+ *  @param background determines if it will be run on the background context (using its background queue) or the main context (using the main thread)
+ *
+ *  @example [myStorage fetchArrayOfModels:[Book class] usingKey:@"title" forIDs:@[@"Gone with the wind", @"The Bible"] inBackground:YES];
+ *  This would search in the background for books with the title "Gone with the wind" or "The Bible".
+ *
+ *  @return array of models that match the criteria
  */
 - (NSArray *)fetchArrayOfModels:(Class)modelType usingKey:(NSString *)key forValues:(NSArray *)valueArray inBackground:(BOOL)background;
 
