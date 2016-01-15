356aac6a51978cea96791093e42bbd457066f060
ryancarlson <ryan.t.carlson@cru.org>
2016-01-15 11:12:23 -0500

    Revert "Use common update notifications,"
    This reverts commit 8489a312d933bde2f4e8672b0c0a56de33356dc0.

--- godtools/godtools-ios-api/GTDataImporter.m
+++ godtools/godtools-ios-api/GTDataImporter.m
@@ -688,9 +688,9 @@ BOOL gtUpdatePackagesUserCancellation									= FALSE;
 		if (language == nil || [currentLanguage.code isEqualToString:language.code]) {
 			
 			[weakSelf downloadPackagesForLanguage:currentLanguage
-                             withProgressNotifier:GTDataImporterNotificationUpdateStarted
-                              withSuccessNotifier:GTDataImporterNotificationUpdateFinished
-                              withFailureNotifier:GTDataImporterNotificationUpdateFailed];
+							 withProgressNotifier:GTDataImporterNotificationMajorUpdateProgressMade
+							  withSuccessNotifier:GTDataImporterNotificationMajorUpdateFinished
+							  withFailureNotifier:GTDataImporterNotificationMajorUpdateFailed];
 		}
 	}];
 	
